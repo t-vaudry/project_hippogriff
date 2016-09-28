9a0bb26db8ef5bfac31dc66a91c57d005447f23a
t-vaudry <tvaudryread@gmail.com>
2016-09-28 15:12:34 -0400

    Working code for 2 species

--- Assignment1/Assignment1/main.cpp
+++ Assignment1/Assignment1/main.cpp
@@ -13,25 +13,27 @@ using namespace std;
 #define X_SIZE 0.001953125
 #define Y_SIZE 0.002604166
 
-char* species;
+char* speciesA;
+char* speciesB;
 
 void glutTimer(int value);
+void display();
 void draw();
-char* initializeGrid();
-char* checkConditions(char* ary);
+void initializeGrid();
+void checkConditions(char* ary);
 int count(int i, int j, char* ary);
 
 int main(int argc, char** argv)
 {
-	species = initializeGrid();
+	initializeGrid();
 	srand(time(NULL));
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(WIDTH, HEIGHT);
 	glutInitWindowPosition(50, 50);
-	glutCreateWindow("OpenGL Setup Test");
+	glutCreateWindow("Game of Life - Multiple Species");
 	glutTimerFunc(33, glutTimer, 1);
-	glutDisplayFunc(draw);
+	glutDisplayFunc(display);
 	glutMainLoop();
 	return 0;
 }
@@ -42,9 +44,20 @@ void glutTimer(int value)
 	glutTimerFunc(33, glutTimer, 1);
 }
 
+void display()
+{
+	thread t1(checkConditions, speciesA);
+	thread t2(checkConditions, speciesB);
+
+	t1.join();
+	t2.join();
+
+	draw();
+	glutSwapBuffers();
+}
+
 void draw()
 {
-	species = checkConditions(species);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glPolygonMode(GL_FRONT_AND_BACK, GL_QUADS);
 	float x;
@@ -60,42 +73,63 @@ void draw()
 			x = -1;
 			y -= Y_SIZE;
 		}
-
 		glBegin(GL_POLYGON);
-		if (species[i*HEIGHT + j] == 'a')
-			glColor3f(1.0f, 1.0f, 1.0f);
-		else if (species[i*HEIGHT + j] == 'd')
-			glColor3f(0.0f, 0.0f, 0.0f);
-		glVertex2f(x, y - Y_SIZE);
-		glVertex2f(x, y);
-		glVertex2f(x + X_SIZE, y);
-		glVertex2f(x + X_SIZE, y - Y_SIZE);
+		
+		if (speciesA[i*HEIGHT + j] != 'n')
+		{
+			
+			if (speciesA[i*HEIGHT + j] == 'a')
+				glColor3f(1.0f, 0.0f, 0.0f);
+			else
+				glColor3f(0.0f, 0.0f, 0.0f);
+		}
+		else
+		{
+			if (speciesB[i*HEIGHT + j] == 'a')
+				glColor3f(0.0f, 0.0f, 1.0f);
+			else
+				glColor3f(0.0f, 0.0f, 0.0f);
+		}
+			glVertex2f(x, y - Y_SIZE);
+			glVertex2f(x, y);
+			glVertex2f(x + X_SIZE, y);
+			glVertex2f(x + X_SIZE, y - Y_SIZE);
 		glEnd();
 		x += X_SIZE;
 	}
-
-	glutSwapBuffers();
 }
 
-char* initializeGrid()
+void initializeGrid()
 {
-	int n = 1; //number of species
-
-	char* ary = new char[786432];
+	speciesA = new char[786432];
+	speciesB = new char[786432];
 
 	for (int i = 0; i<786432; i++)
 	{
 		int type = rand();
-		if (type % 3 == 1)
-			ary[i] = 'a';
-		else
-			ary[i] = 'd';
-	}
+		int status = rand();
+		if (type % 2 == 1)
+		{
+			if (status % 2 == 1)
+				speciesA[i] = 'a';
+			else
+				speciesA[i] = 'd';
 
-	return ary;
+			speciesB[i] = 'n';
+		}
+		else
+		{
+			if (status % 2 == 1)
+				speciesB[i] = 'a';
+			else
+				speciesB[i] = 'd';
+
+			speciesA[i] = 'n';
+		}
+	}
 }
 
-char* checkConditions(char* ary)
+void checkConditions(char* ary)
 {
 	int i = -1;
 	int j = 0;
@@ -108,7 +142,7 @@ char* checkConditions(char* ary)
 			i++;
 		}
 		numNeighbors = count(i, j, ary);
-		
+
 		if (ary[i*HEIGHT + j] == 'a')
 		{
 			switch (numNeighbors)
@@ -128,13 +162,11 @@ char* checkConditions(char* ary)
 				break;
 			}
 		}
-		else if ((ary[i*HEIGHT + j] == 'd')&&(numNeighbors==3))
+		else if ((ary[i*HEIGHT + j] == 'd') && (numNeighbors == 3))
 		{
 			ary[i*HEIGHT + j] = 'a';
 		}
 	}
-
-	return ary;
 }
 
 int count(int i, int j, char* ary)
@@ -237,9 +269,9 @@ int count(int i, int j, char* ary)
 	}
 	else
 	{
-		if (ary[(i-1)*HEIGHT + j + 1] == 'a')
+		if (ary[(i - 1)*HEIGHT + j + 1] == 'a')
 			count++;
-		if (ary[(i-1)*HEIGHT + j] == 'a')
+		if (ary[(i - 1)*HEIGHT + j] == 'a')
 			count++;
 		if (ary[(i - 1)*HEIGHT + j + 1] == 'a')
 			count++;
